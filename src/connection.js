32bd314141e8102b85485d26d6780115b6a25530
Leonardo Oliveros <leonardoliveros.lo@gmail.com>
2017-11-21 01:20:10 -0400

    refactor: Elimina console.log

--- src/connection.js
+++ src/connection.js
@@ -2,18 +2,19 @@ const request = require('request')
 
 const config = require('./config')
 
-const baseUrl = `${config.app.url_api}/invoices`
+const baseUrl = `${config.app.url_api}`
 const credentials = new Buffer(`${config.app.email}:${config.app.token}`).toString('base64')
 const auth = `Basic ${credentials}`
 
 class connection {
-  static request(prefix, method, callback) {
+  static request(prefix, method, body, callback) {
     return request[method]({
-      url: `${baseUrl}${prefix}`,
+      uri: `${baseUrl}${prefix}`,
       headers: {
         Authorization: auth
       },
       json: true,
+      body,
     }, (err, res) => {
       if (err) {
         callback(err, null)
