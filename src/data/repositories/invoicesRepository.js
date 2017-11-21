4cef3072bf726e0d59ee170c048951a83332f69f
Leonardo Oliveros <leonardoliveros.lo@gmail.com>
2017-11-21 01:24:32 -0400

    feat: Agrega metodos updateInvoice y checkedInvoicesDelete

--- src/data/repositories/invoicesRepository.js
+++ src/data/repositories/invoicesRepository.js
@@ -1,14 +1,40 @@
 const connection = require('../../connection')
+const filesRepository = require('./filesRepository')
 
 const prefix = '/invoices'
 
 class invoicesRepository {
   static getInvoices(callback) {
-    return connection.request('/invoices', 'get', (err, res) => {
+    this.checkedInvoicesDelete()
+    return connection.request(prefix, 'get', null, (err, res) => {
       if (err) {
         callback(err, null)
       }
-      callback(null, res)
+
+      const invoices = filesRepository.getInvoices()
+      const newInvoices = res.filter(invoice => !invoices.includes(invoice.id))
+
+      newInvoices.forEach(invoice2 => filesRepository.createFile(`${invoice2.id}.json`, invoice2))
+      callback(null, newInvoices)
+    })
+  }
+
+  static updateInvoice(invoiceId) {
+    const body = { anotation: `Factura eliminada en local el ${new Date()}` }
+    return connection.request(`${prefix}/${invoiceId}`, 'put', body, (err, res) => {
+      if (err) {
+        console.log(err)
+        return false
+      }
+      return res
+    })
+  }
+
+  static checkedInvoicesDelete() {
+    const invoicesDeleted = filesRepository.getInvoicesNotFound()
+    invoicesDeleted.forEach((invoiceDeleted) => {
+      this.updateInvoice(invoiceDeleted)
+      filesRepository.delInvoice(invoiceDeleted)
     })
   }
 }
