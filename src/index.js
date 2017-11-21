a7044dc09c222147a1d179f15f73092377481569
Leonardo Oliveros <leonardoliveros.lo@gmail.com>
2017-11-21 10:53:22 -0400

    refactor: Aumenta tiempo del intervalo a 3min

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const $ = require('jquery')
 const fs = require('fs')
 
-const filenameInvoices = 'invoices'
 let intervalId
 
 const invoicesRepository = require('./data/repositories/invoicesRepository')
@@ -12,7 +11,7 @@ filesRepository.initFileInvoicesDeleted()
 
 // Crea intervalo de 3 min para consultar el API
 // 3min => 180000
-const start = () => intervalId = setInterval(() => invoicesRepository.getInvoices((err, res) => res.forEach(invoice => notify(invoice.total, invoice.client.name))), 10000)
+const start = () => intervalId = setInterval(() => invoicesRepository.getInvoices((err, res) => res.forEach(invoice => notify(invoice.total, invoice.client.name))), 180000)
 
 // Elimina el intervalo
 const stop = () => clearInterval(intervalId)
