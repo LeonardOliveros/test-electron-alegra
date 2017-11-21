95acb7ae41bf42a08aa274872e0ec34f88956e46
Leonardo Oliveros <leonardoliveros.lo@gmail.com>
2017-11-21 01:37:58 -0400

    refactor: Elimina campo innecesario

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,6 @@ dotenv.config()
 
 const config = {
   app: {
-    env: process.env.APP_ENV || 'development',
     email: process.env.EMAIL_ALEGRA || '',
     token: process.env.TOKEN_ALEGRA || '',
     url_api: process.env.URL_API || '',
